76f44d3c44482fbff7b38e0bab7bebefca0fe5f1
narendramehta <narendrasingh.mehta@ziniostech.com>
2020-06-26 18:44:36 +0530

    ++ installed react-simple-chatbot ++ resolved 'react-simple-chatbot' import issue by adding custom declaration 'chatbot-custom' ++ implemented demo chat bot conversations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ChatBot from "react-simple-chatbot";
+
+const steps = [
+  {
+    id: "1",
+    message: "What is your name?",
+    trigger: "2",
+  },
+  {
+    id: "2",
+    user: true,
+    trigger: "3",
+  },
+  {
+    id: "3",
+    message: "Hi {previousValue}, nice to meet you!",
+    end: true,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatBot recognitionEnable={true} steps={steps}></ChatBot>
     </div>
   );
 }
